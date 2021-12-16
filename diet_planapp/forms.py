65decdf14302ce2e91663760d005220a56804d91
lowryel <eugeneagbaglo@gmail.com>
2021-12-16 10:48:55 +0000

    After some new changes

--- diet_planapp/forms.py
+++ diet_planapp/forms.py
@@ -4,4 +4,9 @@ from .models import fill_formModel
 class fill_formForm(ModelForm):
     class Meta:
         model = fill_formModel
-        fields=['age', 'weight', 'height', 'weekly_budget']
+        fields=[
+            'age', 'height', 'weight', 'weekly_budget', 'gender', 'lifestyle', 'goals', 'foods'
+        ]
+
+
+
